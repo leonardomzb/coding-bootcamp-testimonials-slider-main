7ba6b9c827f8710e06018e15a01ce271807d696f
leonardomzb <leonardo.zamorano.b@gmail.com>
2024-06-25 13:46:33 -0400

    Adding sr-only testimony

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,35 @@
-import { quotes } from '../data/quotes.js';
+import { testimonials } from '../data/testimonials.js';
 
-const quoteId = document.getElementById('quote-id');
+const testimonyPositionEl = document.getElementById('quote-position');
 const quoteEL = document.getElementById('quote-section__quote');
 const nameEL = document.getElementById('quote-section__name');
-const profession = document.getElementById('quote-section__profession');
-const imgEl = document.getElementById('quote-section_img');
+const professionEl = document.getElementById('quote-section__profession');
+const imgEl = document.getElementById('quote-section__img');
 const prevButtonEl = document.getElementById('quote-section__prev-button');
 const nextButtonEl = document.getElementById('quote-section__next-button');
-const quoteSectionEl = document.getElementById('quote-section');
-const nav_buttonsEl = document.getElementById('nav_buttons');
 
 nextButtonEl.addEventListener('click', () => { 
-  switchQuote(quotes.length - 1, 0,  1);
+  switchTestimony(testimonials.length - 1, 0,  1);
 });
 
 prevButtonEl.addEventListener('click', () => {
-  switchQuote(0, quotes.length - 1,  -1);
+  switchTestimony(0, testimonials.length - 1,  -1);  
 });
 
-function switchQuote(indexStop, indexReset, indexCall ){
-  for (let i=0; i < quotes.length; i++) {
-    if (quotes[i].id === quoteId.innerHTML) {      
-      if ( i === indexStop) {  
-        getQuoteData(indexReset);
-        break;
-      } else {
-        getQuoteData(i + indexCall);
-        break;
-      } 
-    }
-  } 
+
+function switchTestimony(indexStop, indexReset, indexCall ){
+  if ( (testimonyPositionEl.innerHTML -1 ) === indexStop ) {    
+    getTestimonyData(indexReset);    
+  } else {   
+    getTestimonyData((Number(testimonyPositionEl.innerHTML) - 1) + indexCall);
+  }
 }
 
-function getQuoteData(index){
-  quoteId.innerHTML = quotes[index].id;
-  quoteEL.innerHTML = '" ' + quotes[index].quote + ' "';
-  nameEL.innerHTML = quotes[index].name;
-  profession.innerHTML = quotes[index].profession;  
-  imgEl.src = quotes[index].image;  
+function getTestimonyData(index){
+  testimonyPositionEl.innerHTML = index + 1;
+  quoteEL.innerHTML = '" ' + testimonials[index].quote + ' "';
+  nameEL.innerHTML = testimonials[index].name;
+  professionEl.innerHTML = testimonials[index].profession;  
+  imgEl.src = testimonials[index].image;  
 }
 
